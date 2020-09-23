27fdbd9a62c19196afb508f190bdabf2815ca65a
Utsav Tulsyan <utsav.tulsyan@publicissapient.com>
2020-09-24 02:04:25 +0530

    add logic to main Screen

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    let list = ["C","D","E","F","G","A","B"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1
+           // Code you want to be delayed
+        }
+        playSound(key: list[sender.tag])
+    }
+    
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url!)
+            player?.play()
+        } catch {
+            print("Could not load file")
+        }
+        
+    }
     
 
 }
